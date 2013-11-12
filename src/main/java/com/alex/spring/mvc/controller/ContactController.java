d54eeaab5c57c8a4d5bc5d77aae98abef4799db0
Alexandr Ivanov <alex.ivanoff85@gmail.com>
2013-11-12 18:13:33 +0200

    Add controller

---
+++ src/main/java/com/alex/spring/mvc/controller/ContactController.java
@@ -0,0 +1,34 @@
+package com.alex.spring.mvc.controller;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.alex.spring.mvc.domain.Contact;
+import com.alex.spring.mvc.service.ContactService;
+
+@Controller
+@RequestMapping("/contacts")
+public class ContactController {
+
+	final Logger logg = LoggerFactory.getLogger(ContactController.class);
+	
+	@Autowired
+	private ContactService service;
+	@RequestMapping(method = RequestMethod.GET)
+	public String list(Model uiModel){
+		logg.info("Try to get list of contacts");
+		
+		List<Contact> contacts = service.findAllContacts();
+		uiModel.addAttribute("contacts", contacts);
+		logg.info("Size of contacts is " + contacts.size());
+		
+		return "contacts/list";
+	}
+}
